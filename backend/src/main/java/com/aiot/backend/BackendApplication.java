a49db89899062a18461d9c7965057ba53a0c5422
ngTanPhuc <nguyentanphuc020505@gmail.com>
2026-05-28 21:34:28 +0700

    Debug: add loggers for env var debug

--- backend/src/main/java/com/aiot/backend/BackendApplication.java
+++ backend/src/main/java/com/aiot/backend/BackendApplication.java
@@ -1,7 +1,9 @@
 package com.aiot.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -11,5 +13,13 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
+	@Bean
+    CommandLineRunner debugEnv() {
+		return args -> {
+			System.out.println("SPRING_DATASOURCE_URL = " + System.getenv("SPRING_DATASOURCE_URL"));
+			System.out.println("SPRING_DATASOURCE_USERNAME exists = " + (System.getenv("SPRING_DATASOURCE_USERNAME") != null));
+			System.out.println("SPRING_DATASOURCE_PASSWORD exists = " + (System.getenv("SPRING_DATASOURCE_PASSWORD") != null));
+			System.out.println("JWT_SIGNER_KEY exists = " + (System.getenv("JWT_SIGNER_KEY") != null));
+		};
+	}
 }
