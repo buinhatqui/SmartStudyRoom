f088f9b7267eb33a5738eeaeaaa6b8fe3510c718
ngTanPhuc <nguyentanphuc020505@gmail.com>
2026-05-28 21:42:46 +0700

    Debug: refactor the loggers for env var debug

--- backend/src/main/java/com/aiot/backend/BackendApplication.java
+++ backend/src/main/java/com/aiot/backend/BackendApplication.java
@@ -11,15 +11,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("\uD83E\uDEE6 SPRING_DATASOURCE_URL = " + System.getenv("SPRING_DATASOURCE_URL"));
+		System.out.println("\uD83E\uDEE6 SPRING_DATASOURCE_USERNAME exists = " + (System.getenv("SPRING_DATASOURCE_USERNAME") != null));
+		System.out.println("\uD83E\uDEE6 SPRING_DATASOURCE_PASSWORD exists = " + (System.getenv("SPRING_DATASOURCE_PASSWORD") != null));
+		System.out.println("\uD83E\uDEE6 JWT_SIGNER_KEY exists = " + (System.getenv("JWT_SIGNER_KEY") != null));
+		System.out.println("\uD83E\uDEE6 PORT = " + System.getenv("PORT"));
+
 		SpringApplication.run(BackendApplication.class, args);
-	}
-	@Bean
-    CommandLineRunner debugEnv() {
-		return args -> {
-			System.out.println("SPRING_DATASOURCE_URL = " + System.getenv("SPRING_DATASOURCE_URL"));
-			System.out.println("SPRING_DATASOURCE_USERNAME exists = " + (System.getenv("SPRING_DATASOURCE_USERNAME") != null));
-			System.out.println("SPRING_DATASOURCE_PASSWORD exists = " + (System.getenv("SPRING_DATASOURCE_PASSWORD") != null));
-			System.out.println("JWT_SIGNER_KEY exists = " + (System.getenv("JWT_SIGNER_KEY") != null));
-		};
 	}
 }
